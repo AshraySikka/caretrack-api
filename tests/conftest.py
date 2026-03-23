774bb9d1a4a5d7bced0ef5b5f7580834a577178e
Ashray Sikka <ashray15.sikka@gmail.com>
2026-03-23 12:34:14 -0400

    test: add auth endpoint tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import asyncio
-import pytest
+import pytest #noqa
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
